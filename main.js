7ea7e686e6957648d06aa067a1c0929e8dab7f68
alexstewart96 <s1507747@student.southstaffs.ac.uk>
2017-04-27 11:18:43 +0100

    main updated

--- main.js
+++ main.js
@@ -1,23 +1,59 @@
 //create our main state that will contain the game
 var mainStage = {
-    preload: function() {}
+    preload: function() {
     //This function will be exicuted at the beginning 
     //thats where we load the images and sound
-   },
+   
 
+    //load the bird sprite
+    game.load.image('bird', 'assets/bird.png');
+    
+    },
      create: function() {
       //This function is called after the preload function
       //here we setup the game, display sprites
     
-    
+      //change the backgrouns colour of the game to blue - for now
+         game.stage.backgroundColor = '#71c5cf';
+         
+         //set the physics for the game
+         game.physics.startsystem(Phaser.Physics.ARCADE);
+         
+         //display the bird at the position of x=100 and y=245
+         this.bird = game.add.sprite(100, 245, 'bird');
+         
+         //add physics to the bird
+         //needed for:movement gravity and collisions, etc.
+         game.physics.arcade.enable(this.bird);
+         
+         //add gravity to the bird to make it fall
+         this.bird.body.gravity.y = 1000;
+         
+         //call 'jump' function when the spacebar is pressed
+         var spaceBar = game.input.keyboard.addkey(                                                                                                                              phaser.keyboard.spaceBar);
+         spaceBar.onDown.add(this.jump, this);
     },
         
        update: function () {
             //This function is called 60 times per second
             //It contains the games logic
         
+           //call the 'restartgame' function
+           if (this.bird.y <0 || this.bird.y > 490)
+               this.restartGame();
 },
     
+    jump: function() {
+        
+        //Add a vertical velocity to the bird
+        this.bird.body.velocity.y = -350;
+    },
+    
+    //restartthe game
+    restartGame: function() {
+    //start the main state which restarts the game
+    game.state.start('main');
+},
 };
 
 //initialise phaser and create a 400px x 490px game

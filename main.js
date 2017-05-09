0daa6ae6bfb933b0a538755d435ba89610a0ac95
alexstewart96 <s1507747@student.southstaffs.ac.uk>
2017-05-09 09:53:32 +0100

    minor update

--- main.js
+++ main.js
@@ -138,7 +138,7 @@ this.labelScore.text = this.score;
 
 };
 //initialise phaser and create a 400px x 490px game
-var game = new Phaser.game(400, 490);
+var game = new Phaser.Game(400, 490);
 
 //add the mainstate and call it main
 game.state.add('main', mainState);
